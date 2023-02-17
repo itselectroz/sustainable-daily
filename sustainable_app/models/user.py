f313fe0dd854759b77afdfa8e84b529b9dbe606f
itselectroz <harry.whittle@cwgsy.net>
2023-02-17 16:29:21 +0000

    Refactor

--- sustainable_app/models/user.py
+++ sustainable_app/models/user.py
@@ -2,8 +2,9 @@ import math
 
 from django.db import models
 
+
 class User(models.Model):
-    first_name = models.CharField(max_length=200) # change max length for these
+    first_name = models.CharField(max_length=200)  # change max length for these
     last_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
 
@@ -14,22 +15,21 @@ class User(models.Model):
     # Account stuff
     xp = models.IntegerField(default=0)
     points = models.IntegerField(default=0)
-    
-    weekly_xp = models.IntegerField(default=0) # gets reset every monday
 
+    weekly_xp = models.IntegerField(default=0)  # gets reset every monday
 
     # owned_items = models.ManyToManyField(Item)
     # equipped_items = models.ManyToManyField(Item)
-    
+
     # Personal goals is a relation that will be in goal maybe?
 
-    ### Methods
+    # Methods
 
     def username(self):
         return f"{self.first_name} {self.last_name}"
-    
+
     def level(self):
         return math.floor(0.07 * math.sqrt(self.xp))
-    
+
     def xp_for_level(self, level):
         return math.floor((level / 0.07) ** 2)
