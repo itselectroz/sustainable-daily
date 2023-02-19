aefad2a4a3d52961994313a49cf5075cf1699562
itselectroz <harry.whittle@cwgsy.net>
2023-02-19 11:34:24 +0000

    Refactor user model and create tests

---
+++ sustainable_app/tests/models/user_test.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+
+from ...models import User
+
+class UserModelTests(TestCase):
+    def test_level_with_no_xp(self):
+       """
+       Checks the level method returns 0 when you have no xp
+       """
+       user = User()
+       user.xp = 0
+       self.assertEqual(user.level(), 0)
+
+    def test_level_with_some_xp(self):
+       """
+       Checks the level method returns 10 when you have 20408 xp
+       """
+       user = User()
+       user.xp = 20408
+       self.assertEqual(user.level(), 10)
+
+    def test_xp_for_level_with_level_0(self):
+       """
+       Checks the xp_for_level method returns 0 for level 0
+       """
+       user = User()
+       self.assertEqual(user.xp_for_level(0), 0)
+       
+    def test_xp_for_level_with_level_20(self):
+       """
+       Checks the xp_for_level method returns 81632 for level2 0
+       """
+       user = User()
+       self.assertEqual(user.xp_for_level(20), 81632)
