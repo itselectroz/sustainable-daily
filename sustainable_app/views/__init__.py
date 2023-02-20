c9627f4c0334045bbd5c31087cccf8442cdb8e82
itselectroz <harry.whittle@cwgsy.net>
2023-02-20 16:13:35 +0000

    Changes

--- sustainable_app/views/__init__.py
+++ sustainable_app/views/__init__.py
@@ -2,8 +2,9 @@
 # add all names to __all__ array
 
 from .index import index
+from .home import home
 from .login import login
 from .profile import profile
 from .register import register
 
-__all__ = ['index', 'login', 'profile', 'register']
+__all__ = ['index', 'login', 'profile', 'register', 'home']
