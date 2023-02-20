8844426af08d860ee647812edc19f36065105e5f
Josh-Cox <joshcox2@hotmail.com>
2023-02-20 21:40:07 +0000

    started rubbish falling

--- sustainable_app/static/sustainable_app/minigames/catching_game/app_test.js
+++ sustainable_app/static/sustainable_app/minigames/catching_game/app_test.js
@@ -2,6 +2,9 @@
 // Define html elements
 const container = document.getElementById("container");
 const player = document.getElementById("player");
+const rubbish_group = document.querySelector(".rubbish_group");
+const playerLeft = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
+const playerBottom = parseInt(window.getComputedStyle(player).getPropertyValue("bottom"));
 
 // Define X value
 let x = 0;
@@ -34,4 +37,34 @@ function getOffest(el) {
     return rect.left + window.scrollX;
 }
 
+function generateRubbish() {
+    let rubbishBottom = container.offsetHeight;
+    let rubbishLeft = Math.floor(Math.random() * container.offsetWidth);
+    let rubbish = document.createElement('div');
+    rubbish.setAttribute("class", "rubbish");
+    rubbish_group.appendChild(rubbish);
+
+    function fallDown() {
+
+        if(rubbishBottom == playerBottom && rubbishLeft > playerLeft && rubbishLeft < playerLeft + player.offsetWidth) {
+            rubbish_group.removeChild(rubbish);
+            clearInterval(fallInterval);
+        }
+        if(rubbishBottom < playerBottom) {
+            alert("Game Over! Score:");
+            clearInterval(fallInterval);
+            clearTimeout(rubbishTimeout);
+            location.reload();
+        }
+        rubbishBottom -= 10;
+        rubbish.style.bottom = rubbishBottom + 'px';
+    }
+
+    let fallInterval = setInterval(fallDown, 20);
+    let rubbishTimeout = setTimeout(generateRubbish, 2000);
+    rubbish.style.bottom = rubbishBottom + 'px';
+    rubbish.style.left = rubbishLeft + 'px';
+}
+
+generateRubbish();
 updateFrame();
