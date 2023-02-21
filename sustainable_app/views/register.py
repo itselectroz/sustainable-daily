02b0605f3be202158e95883a17e1b6c6f13e4798
itselectroz <harry.whittle@cwgsy.net>
2023-02-21 11:13:20 +0000

    Registration changes, waiting for refactor first

--- sustainable_app/views/register.py
+++ sustainable_app/views/register.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import login
 
@@ -5,18 +6,34 @@ from ..models import User
 
 
 def register_user(request):
+    issues = []
+
+    for name in ['username', 'password', 'first-name', 'last-name', 'email']:
+        if not request.POST.get(name, ''):
+            issues.append(name)
+
+    if len(issues) > 0:
+        # TODO: once register view has been themeified by oscar, this error stuff needs to be reimplemented
+        return render(request, 'sustainable_app/register.html', {
+            'error': True,
+            'error_message': 'Missing required information.',
+            'issues': issues
+        })
+
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
     first_name = request.POST.get('first-name', '')
     last_name = request.POST.get('last-name', '')
     email = request.POST.get('email', '')
 
-    for var in [username, password, first_name, last_name, email]:
-        if not var:
-            # TODO: better error here
-            return redirect(reverse('register') + "?error=1")
-
-    user = User.objects.create_user(username, email, password)
+    try:
+        user = User.objects.create_user(username, email, password)
+    except IntegrityError:
+        return render(request, 'sustainable_app/register.html', {
+            'error': True,
+            'error_message': 'Account with username already exists.',
+            'issues': []
+        })
 
     user.first_name = first_name
     user.last_name = last_name
@@ -35,6 +52,16 @@ def register(request):
         return redirect(reverse('profile'))
 
     if request.method == "POST" and request.POST is not None:
-        return register_user(request)
+        try:
+            return register_user(request)
+        except Exception:
+            return render(request, 'sustainable_app/register.html', {
+                'error': True,
+                'error_message': 'Something went wrong, please try again.',
+                'issues': []
+            })
 
-    return render(request, 'sustainable_app/register.html')
+    return render(request, 'sustainable_app/register.html', {
+        'error': False,
+        'issues': []
+    })
