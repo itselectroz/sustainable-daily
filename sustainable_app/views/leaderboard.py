019c648d714eca26d7026c9601265699857fbaa2
FieldMarshalGaig <mooresoscar1@gmail.com>
2023-02-24 22:51:31 +0000

    leaderboard can be ordered
    uses a get request, probably need to find a better way of doing it

--- sustainable_app/views/leaderboard.py
+++ sustainable_app/views/leaderboard.py
@@ -4,11 +4,48 @@ from django.template import loader
 from sustainable_app.models.user import User
 
 
+
 def leaderboard(request):
 
     template = loader.get_template("sustainable_app/leaderboard.html")
 
-    # Figure out how to get names for leaderboard
-    names = User.objects.order_by("-xp")
-    context = {"names": names}
-    return HttpResponse(template.render(context))
+    if request.method == 'GET':
+        param = request.GET.get("param",None)
+        
+        if param == "username" :
+            context = sorted_by_username()
+        elif param == "level":
+            context =  sorted_by_xp()
+        elif param == "points":
+            context = sorted_by_points()
+        else:
+            return HttpResponse(template.render(sorted_by_username()))
+
+        return HttpResponse(template.render(context))
+
+    else:
+        return HttpResponse(template.render(sorted_by_username()))
+   
+    
+
+# Returns dictionary sorted by username
+def sorted_by_username():
+    
+    users = User.objects.order_by("username")
+    context = {"users": users}
+    return context
+
+# Returns dictionary sorted by xp
+def sorted_by_xp():
+    
+    users = User.objects.order_by("-xp")
+    context = {"users": users}
+    return context
+
+# Returns dictionary sorted by level
+def sorted_by_points():
+    
+    users = User.objects.order_by("-points")
+    context = {"users": users}
+    return context
+
