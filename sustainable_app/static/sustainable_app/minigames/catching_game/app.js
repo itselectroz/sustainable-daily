49e3839755b0596843abde5e645c803fe9939ca6
Josh <joshua@durhamgardens.co.uk>
2023-02-25 15:40:59 +0000

    deleted unused folder

--- sustainable_app/static/sustainable_app/minigames/catching_game/app.js
+++
@@ -1,8 +0,0 @@
-let game_container = document.querySelector(".game-container");
-let bin = document.querySelector(".bin");
-let rubbish_group = document.querySelector(".rubbish_group");
-let binLeft = parseInt(window.getComputedStyle(bin).getPropertyValue("left"));
-let binBottom = parseInt(window.getComputedStyle(bin).getPropertyValue("bottom"));
-let score = 0;
-
-
