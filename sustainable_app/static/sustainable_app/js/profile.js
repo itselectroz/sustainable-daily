f8bc98dae26d90c11bb71da0d986879ebe76f4a4
Dora-Napier <doranapier3@gmail.com>
2023-02-25 15:38:20 +0000

    Added background-color ajax request

--- sustainable_app/static/sustainable_app/js/profile.js
+++ sustainable_app/static/sustainable_app/js/profile.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    // Request for name color
     $('#name_color input').on('change', function() {
 
         $name_color = $('input[name="name_color_select"]:checked').val();
@@ -7,7 +9,28 @@ $(document).ready(function() {
             type: "POST",
             url: "equip/",
             data: {
-                username_color: $name_color,
+                type: "username_color",
+                name: $name_color,
+                csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val(),
+            },
+            success: function() {
+                alert("Equipped");
+                window.location.reload();
+            }
+        });
+    });
+
+    // Request for background color
+    $('#background_color input').on('change', function() {
+
+        $background_color = $('input[name="background_select"]:checked').val();
+
+        $.ajax({
+            type: "POST",
+            url: "equip/",
+            data: {
+                type: "background_color",
+                name: $background_color,
                 csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val(),
             },
             success: function() {
--- sustainable_app/views/profile.py
+++ sustainable_app/views/profile.py
@@ -12,10 +12,10 @@ def profile(request):
     template = loader.get_template("sustainable_app/profile.html")
 
     current_user = request.user
-    #current_user.equipped_items.add(1)
 
     # get user attributes
     user_name_color = current_user.equipped_items.get(type="username_color")
+    background_color = current_user.equipped_items.get(type="background_color")
 
     # TODO: Caculate xp needed for next level
 
@@ -28,25 +28,36 @@ def profile(request):
     # pass user attributes to template
     context = {"user": current_user,
                "text_color": user_name_color,
+               "background_color": background_color,
                "user_level":user_level,
                }
 
     return render(request, "sustainable_app/profile.html", context)
 
+# Equip request
 def equip(request):
 
     current_user = request.user
-    username_color = request.POST.get('username_color', False)
 
+    # Get the type and name of item
+    type = request.POST.get('type', False)
+    name = request.POST.get('name', False)
+
+    # Pass to function
+    changeAccessory(type, name, current_user)
+
+    return redirect('/')
+
+# Change accessory
+def changeAccessory(type, name, current_user):
+    
     ## find item to remove and remove it
     for item_to_remove in Item.objects.all():
-        if item_to_remove.type == "username_color":
+        if item_to_remove.type == type:
             current_user.equipped_items.remove(item_to_remove.id)
 
     ## find item to add and add it
     for item_to_add in Item.objects.all():
-        if item_to_add.type == "username_color":
-            if item_to_add.name == str(username_color):
+        if item_to_add.type == type:
+            if item_to_add.name == str(name):
                 current_user.equipped_items.add(item_to_add.id)
-
-    return redirect('/')
