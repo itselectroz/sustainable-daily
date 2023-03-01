818b0c36cef12ba2dc9fb1890bbaa86e20fd9bf2
Josh-Cox <joshcox2@hotmail.com>
2023-03-01 13:29:02 +0000

    Hardcoded values for prototype

--- sustainable_app/static/sustainable_app/js/home.js
+++ sustainable_app/static/sustainable_app/js/home.js
@@ -1,37 +1,116 @@
+
+// Base strings
+const GOALS_TEXT_PREFIX = "Completed: ";
+const GOALS_TEXT_SUFFIX = "/5";
+
+// Define main html elements
 const goal_items = document.querySelectorAll(".item");
-const click_effects = document.querySelectorAll(".click-effect");
-const goal1 = document.getElementById("u-one");
-const goal2 = document.getElementById("u-two");
+const u_goals_text = document.getElementById("universal-text");
+const p_goals_text = document.getElementById("personal-text");
 
-goal1.style.backgroundImage = "url(/static/sustainable_app/img/test_home_tile.jpg)";
+// Individual universal goals
+const u_goal1 = document.getElementById("u-one");
+const u_goal2 = document.getElementById("u-two");
+const u_goal3 = document.getElementById("u-three");
+const u_goal4 = document.getElementById("u-four");
+const u_goal5 = document.getElementById("u-five");
 
-let u_goals_urls = ["/minigame_catching", "#", "#", "#", "#"];
-let u_goals_complete = ["green", "grey", "grey", "grey", "grey"];
+// Individual personal goals
+const p_goal1 = document.getElementById("p-one");
+const p_goal2 = document.getElementById("p-two");
+const p_goal3 = document.getElementById("p-three");
+const p_goal4 = document.getElementById("p-four");
+const p_goal5 = document.getElementById("p-five");
 
-//TODO: implement images
-let u_goals_type = ["game", "qr", "quiz", "survey"];
+// Urls and types for universal goals
+let goals_urls = ["/minigame_catching", "#", "#", "#", "#"];
+let u_goals_type = ["catching-game", "sorting-game", "qr", "quiz", "survey", "wordle"]; // TODO: Implement auto selected images
 
-// redirects
-for(let i = 0; i < goal_items.length; i++) {
-    goal_items[i].addEventListener("click", () => {
-        window.location.replace(u_goals_urls[i]);
+
+// Set backgrounds for universal goals (hardcoded temporarily)
+u_goal1.style.backgroundImage = "url(/static/sustainable_app/img/test_home_tile.jpg)";
+u_goal2.style.backgroundImage = "url(/static/sustainable_app/img/test_home_tile.jpg)";
+u_goal3.style.backgroundImage = "url(/static/sustainable_app/img/qr.png)";
+u_goal4.style.backgroundImage = "url(/static/sustainable_app/img/qr.png)";
+u_goal5.style.backgroundImage = "url(/static/sustainable_app/img/qr.png)";
+
+// Set background for personal goals (hardcoded temporarily)
+p_goal1.style.backgroundImage = "url(/static/sustainable_app/img/workout.png)";
+p_goal2.style.backgroundImage = "url(/static/sustainable_app/img/plastic_bottle.png)";
+p_goal3.style.backgroundImage = "url(/static/sustainable_app/img/cat_recycle.png)";
+p_goal4.style.backgroundImage = "url(/static/sustainable_app/img/paper.png)";
+p_goal5.style.backgroundImage = "url(/static/sustainable_app/img/watermelon.png)";
+
+
+function setGoalText() {
+    let u_num = 0;
+    let p_num = 0;
+
+    // Increment each number
+    goal_items.forEach(item => {
+
+        if(item.getAttribute("value") == "var(--completion-green)") {
+
+            switch (item.getAttribute("name")) {
+                case "universal":
+                    u_num++;
+                    break;
+
+                case "personal":
+                    p_num++;
+                    break;
+            }
+        }
     });
-};
 
-// button click effect
-for(let i = 0; i < goal_items.length; i++) {
+    // Display numbers
+    u_goals_text.innerText = GOALS_TEXT_PREFIX + u_num + GOALS_TEXT_SUFFIX
+    p_goals_text.innerText = GOALS_TEXT_PREFIX + p_num + GOALS_TEXT_SUFFIX
+}
 
-    goal_items[i].addEventListener("touchstart", () => {
-        goal_items[i].style.transform = "scale(80%, 80%)";
+
+function setRedirects() {
+    // redirects
+    for(let i = 0; i < goal_items.length; i++) {
+        goal_items[i].addEventListener("click", () => {
+
+            if(goal_items[i].getAttribute("name") == "personal") {
+                goal_items[i].setAttribute("value", "var(--completion-green)");
+                loadColors();
+                setGoalText();
+            }
+            else {
+                window.location.replace(goals_urls[i]);
+            }
+        });
+    };
+}
+
+
+function setClickEffects() {
+    // button click effect
+    for(let i = 0; i < goal_items.length; i++) {
+
+        goal_items[i].addEventListener("touchstart", () => {
+            goal_items[i].style.transform = "scale(80%, 80%)";
+        });
+
+        goal_items[i].addEventListener("touchend", () => {
+            goal_items[i].style.transform = "scale(100%, 100%)";
+        });
+    };
+}
+
+
+function loadColors() {
+    // setting completed color
+    goal_items.forEach(item => {
+        item.style.setProperty("--background-completed", item.getAttribute("value"));
     });
+}
 
-    goal_items[i].addEventListener("touchend", () => {
-        goal_items[i].style.transform = "scale(100%, 100%)";
-    });
-};
-
-
-// setting completed color
-goal_items.forEach(item => {
-    item.style.setProperty('--background-completed', item.getAttribute('value'));
-});
+// Initialize screen
+setRedirects();
+setClickEffects();
+loadColors();
+setGoalText();
