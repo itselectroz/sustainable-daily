9d99245d001ff4e9869b7505efa1bff5290f30aa
Josh <joshua@durhamgardens.co.uk>
2023-03-10 13:37:19 +0000

    question model created

--- sustainable_app/models/__init__.py
+++ sustainable_app/models/__init__.py
@@ -5,5 +5,6 @@
 from .goal import Goal
 from .item import Item
 from .user import User
+from .question import Question
 
-__all__ = ['User', 'Item', 'Goal']
+__all__ = ['User', 'Item', 'Goal', Question]
--- sustainable_app/static/sustainable_app/js/quiz.js
+++ sustainable_app/static/sustainable_app/js/quiz.js
@@ -15,12 +15,32 @@ const menuButton = document.getElementById("btnMenu")
 
 menuText.innerText = "The Ultimate Sustainability Quiz"
 
+// Set variables
+let score = 0
+let questionNum = -1
+
+let questions = {"question":"test", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4"}
+
+/**
+ * Display new question
+ */
+function createNewQuestion() {
+    questionNum += 1
+    let newQuestion = questions[questionNum]
+    question.textContent = newQuestion["question"]
+    label1.textContent = newQuestion["a1"]
+    label2.textContent = newQuestion["a2"]
+    label3.textContent = newQuestion["a3"]
+    label4.textContent = newQuestion["a4"]
+}
+
 /**
  * Start quiz
  */
 function startGame() {
     menuScreen.style.display = "none"
     container.style.display = "flex"
+    createNewQuestion()
 }
 
 /**
