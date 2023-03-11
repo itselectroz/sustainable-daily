228f47289432ebe42fc133dcd8d711a469a212d5
Josh <joshua@durhamgardens.co.uk>
2023-03-11 13:36:15 +0000

    quiz made

--- sustainable_app/static/sustainable_app/js/quiz.js
+++ sustainable_app/static/sustainable_app/js/quiz.js
@@ -12,26 +12,107 @@ const label4 = document.querySelector('label[for="answer4"]')
 const menuScreen = document.getElementById("menu")
 const menuText = document.getElementById("menuText")
 const menuButton = document.getElementById("btnMenu")
+const button = document.querySelector(".confirm-button")
+const results = document.querySelector(".results")
+const resultText = document.querySelector(".result-text")
+const questionContainer = document.querySelector(".question-container")
+const nextButton = document.querySelector(".next-button")
 
+// Set defaults
 menuText.innerText = "The Ultimate Sustainability Quiz"
 
 // Set variables
 let score = 0
 let questionNum = -1
+let correctAns = null
 
-let questions = {"question":"test", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4"}
+/**
+ * Check if answer was correct
+ */
+function checkAns() {
+    if (ans1.checked) {
+        if (correctAns == 1) {
+            correctAnswer(label1.textContent)
+        } else {
+            wrongAnswer(getCorrectAnswer())
+        }
+    } else if (ans2.checked) {
+        if (correctAns == 2) {
+            correctAnswer(label2.textContent)
+        } else {
+            wrongAnswer(getCorrectAnswer())
+        }
+    } else if (ans3.checked) {
+        if (correctAns == 3) {
+            correctAnswer(label3.textContent)
+        } else {
+            wrongAnswer(getCorrectAnswer())
+        }
+    } else if (ans4.checked) {
+        if (correctAns == 4) {
+            correctAnswer(label4.textContent)
+        } else {
+            wrongAnswer(getCorrectAnswer())
+        }
+    }
+}
+
+/**
+ * Gets the string representation of the correct answer
+ * @returns the correct answer
+ */
+function getCorrectAnswer() {
+    switch (correctAns) {
+        case 1:
+            return label1.textContent
+        case 2:
+            return label2.textContent
+        case 3:
+            return label3.textContent
+        case 4:
+            return label4.textContent
+        default:
+            return null
+    }
+}
+
+/**
+ * Handle when the player gets a question right
+ * @param {String} answer 
+ */
+function correctAnswer(answer) {
+    resultText.style.color = "green"
+    resultText.textContent = "Correct! The answer is in fact " + answer + "!"
+    score += 1
+    questionContainer.style.display = "none"
+    results.style.display = "flex"
+}
+
+function wrongAnswer(correctAnswer) {
+    resultText.style.color = "red"
+    resultText.textContent = "Incorrect - the corret answer is actually " + correctAnswer + "!"
+    questionContainer.style.display = "none"
+    results.style.display = "flex"
+}
 
 /**
  * Display new question
  */
 function createNewQuestion() {
     questionNum += 1
-    let newQuestion = questions[questionNum]
-    question.textContent = newQuestion["question"]
-    label1.textContent = newQuestion["a1"]
-    label2.textContent = newQuestion["a2"]
-    label3.textContent = newQuestion["a3"]
-    label4.textContent = newQuestion["a4"]
+    console.log(questionNum)
+    let newQuestion = questions[questionNum].fields
+    question.textContent = newQuestion.question
+    label1.textContent = newQuestion.a1
+    label2.textContent = newQuestion.a2
+    label3.textContent = newQuestion.a3
+    label4.textContent = newQuestion.a4
+    correctAns = newQuestion.correct_answer
+
+    // Change button text
+    if (questionNum >= questions.length-1) {
+        nextButton.textContent = "End Quiz"
+    }
 }
 
 /**
@@ -40,7 +121,21 @@ function createNewQuestion() {
 function startGame() {
     menuScreen.style.display = "none"
     container.style.display = "flex"
+    questionContainer.style.display = "flex"
+    results.style.display = "none"
     createNewQuestion()
+}
+
+/**
+ * End quiz
+ */
+function endGame() {
+    menuScreen.style.display = "flex"
+    container.style.display = "none"
+    menuButton.textContent = "Play Again"
+    menuText.innerText = "Quiz Finished!\nScore: " + score + " out of " + questions.length
+    score = 0
+    questionNum = -1
 }
 
 /**
@@ -48,4 +143,25 @@ function startGame() {
  */
 function exitGame() {
     window.location.replace("/home");
+}
+
+/**
+ * Sets up for next question
+ */
+function nextQuestion() {
+    // Check if on final question
+    if (questionNum >= questions.length-1) {
+        endGame()
+        return
+    }
+
+    createNewQuestion()
+    questionContainer.style.display = "flex"
+    results.style.display = "none"
+
+    // Clear radio buttons
+    ans1.checked = "false"
+    ans2.checked = "false"
+    ans3.checked = "false"
+    ans4.checked = "false"
 }
--- sustainable_app/views/quiz.py
+++ sustainable_app/views/quiz.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
 from django.shortcuts import render
+from sustainable_app.models import Question
 
 #@login_required(login_url='/login')
 def quiz(request):
-    return render(request, 'sustainable_app/quiz.html')
+    # Get up to 5 random questions
+    questions = Question.objects.order_by('?')[:5]
+
+    questions = serializers.serialize('json', questions)
+
+    context = {
+        "questions":questions
+    }
+
+    return render(request, 'sustainable_app/quiz.html', context=context)
