a791c2a68e2606b9cf871b5fdf0c21addfdcea15
Josh-Cox <joshcox2@hotmail.com>
2023-03-11 13:50:02 +0000

    updated __init__

--- sustainable_app/tests/__init__.py
+++ sustainable_app/tests/__init__.py
@@ -3,5 +3,6 @@
 # add all names to __all__ array
 
 from .models import UserModelTests
+from .models import LocationModelTests
 
-__all__ = ['UserModelTests']
+__all__ = ['UserModelTests', 'LocationModelTests']
