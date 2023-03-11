8886470a7a2a00ab698bb7160f369f4483eccbb9
itselectroz <harry.whittle@cwgsy.net>
2023-03-11 15:06:58 +0000

    Refactored game_keeper.py

--- sustainable_app/views/game_keeper.py
+++ sustainable_app/views/game_keeper.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.core.files import File
+from django.http import HttpResponse
 
 from io import BytesIO
 
@@ -8,35 +9,43 @@ from sustainable_app.models.location import Location
 import qrcode
 
 # game keeper page
+
+
 def game_keeper(request):
-    
     if request.method == "POST" and request.POST is not None:
         return locations_add(request)
-    
+
     # send all game keepers to template
     context = {
         "game_keepers":  User.objects.filter(game_keeper=True)
     }
-    
-    return direct_user("", request, context) 
-   
-# game keeper locations page
+
+    return direct_user("", request, context)
+
+
 def game_keeper_locations(request):
-    
     # send all locations to template
     context = {
         "locations":  Location.objects.all()
     }
     return direct_user("_locations", request, context)
 
+
 def game_keeper_surveys(request):
     return direct_user("_surveys", request, {})
+
 
 def game_keeper_events(request):
     return direct_user("_events", request, {})
 
-# Remove keeper request
+
 def remove_keeper(request):
+    """
+    View for removing a game keeper
+    """
+    if (not request.user.is_authenticated or not request.user.game_keeper):
+        return HttpResponse('Unauthorized', status=401)
+
     # get username from post request
     username = request.POST.get('username', False)
     # delete user with given username
@@ -45,70 +54,77 @@ def remove_keeper(request):
     return redirect('/')
 
 # add a location
-def locations_add(request):
-    if(request.user.is_authenticated and request.user.game_keeper):
-        # if there is a post request
-        if request.method == 'POST':
-            # get location attributes from post request
-            name = request.POST.get('name', '')
-            category = request.POST.get('category', '')
-            clue = request.POST.get('clue', '')
-            image = request.POST.get('image', '')
-            
-            # create location object
-            new_location = Location(name=name, category=category, clue=clue)
-            new_location.save()
-            
-            # generate qr code
-            qr = qrcode.QRCode(
-                version=1,
-                error_correction=qrcode.constants.ERROR_CORRECT_L,
-                box_size=10,
-                border=4,
-            )
-            
-            #TODO: When deployed change data to url
-            qr.add_data('127.0.0.1:8000/location_qr/' + str(new_location.id))
-            qr.make(fit=True)
 
-            img = qr.make_image(fill_color="black", back_color="white")
-            
-            buffer = BytesIO()
-            img.save(buffer, format='PNG')
-            
-            # set attributes
-            new_location.image = request.FILES['image']
-            new_location.qr.save(f"qr_{new_location.id}.png", File(buffer))
-        
-            return redirect(reverse('game_keeper_locations'))
-        else:
-            return redirect(reverse('home'))
+
+def locations_add(request):
+    if (not request.user.is_authenticated or not request.user.game_keeper):
+        return HttpResponse('Unauthorized', status=401)
+
+    if (request.method != 'POST'):
+        # redirect to home if it's not a post request
+        return redirect(reverse('home'))
+
+    # get location attributes from post request
+    name = request.POST.get('name', '')
+    category = request.POST.get('category', '')
+    clue = request.POST.get('clue', '')
+
+    # create location object
+    new_location = Location(name=name, category=category, clue=clue)
+    new_location.save()
+
+    # generate qr code
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    # TODO: When deployed change data to url
+    qr.add_data('127.0.0.1:8000/location_qr/' + str(new_location.id))
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    buffer = BytesIO()
+    img.save(buffer, format='PNG')
+
+    # set attributes
+    new_location.image = request.FILES['image']
+    new_location.qr.save(f"qr_{new_location.id}.png", File(buffer))
+
+    return redirect(reverse('game_keeper_locations'))
 
 # remove a location
+
+
 def locations_remove(request):
-    
-    # if user is authenticated
-    if(request.user.is_authenticated):
-        # get location id from post request
-        location_id = request.POST.get('location_id', False)
-        # delete user with given username
-        Location.objects.get(id=location_id).delete()
-        # refresh the page
-        return redirect(reverse('game_keeper_locations'))
+    if (not request.user.is_authenticated or not request.user.game_keeper):
+        return HttpResponse('Unauthorized', status=401)
 
-# direct user to correct page
+    # get location id from post request
+    location_id = request.POST.get('location_id', False)
+    # delete user with given username
+    Location.objects.get(id=location_id).delete()
+    # refresh the page
+    return redirect(reverse('game_keeper_locations'))
+
+
 def direct_user(page, request, context):
+    """
+    Function for directing user to correct page
+    based on authentication and authorization
+    """
     # if user is authenticated
-    if(request.user.is_authenticated):
-        # if user is a game keeper
-        if(request.user.game_keeper):
-            # render game keeper locations page
-            return render(request, 'sustainable_app/game_keeper' + page + '.html', context)
-        else:
-            # render home page
-            return redirect(reverse('home'))
-    else:
-        # render login
+    if (not request.user.is_authenticated):
+        # redirect to login
         return redirect(reverse('login'))
-    
 
+    # if user is not a game keeper
+    if (not request.user.game_keeper):
+        # redirect to home page
+        return redirect(reverse('home'))
+
+    # render game keeper locations page
+    return render(request, 'sustainable_app/game_keeper' + page + '.html', context)
