98a4e22752ada1796a63d44baf04e7252b6fa5a9
Dora-Napier <doranapier3@gmail.com>
2023-03-12 13:21:28 +0000

    can complete personal goals

--- sustainable_app/views/__init__.py
+++ sustainable_app/views/__init__.py
@@ -2,7 +2,7 @@
 # add all names to __all__ array
 
 from .index import index
-from .home import home
+from .home import home, complete_personal
 from .login import login
 from .logout import logout
 from .profile import profile, equip
@@ -16,4 +16,4 @@ from .game_keeper import game_keeper, game_keeper_locations, game_keeper_surveys
 __all__ = ['index', 'login', 'logout', 'profile', 'register', 'leaderboard', 'home', 
            'minigame_catching', 'sorting', 'equip', 'game_keeper', 
            'game_keeper_locations', 'game_keeper_surveys', 'game_keeper_events', 'remove_keeper'
-           'locations_add', 'locations_remove', 'qr_callback']
+           'locations_add', 'locations_remove', 'qr_callback', 'complete_personal']
