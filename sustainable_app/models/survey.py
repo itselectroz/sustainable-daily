708f21adb397a5c6bf792e276d44862ee0016ef2
itselectroz <harry.whittle@cwgsy.net>
2023-03-14 18:44:17 +0000

    Tidy up code

--- sustainable_app/models/survey.py
+++ sustainable_app/models/survey.py
@@ -1,10 +1,10 @@
 from django.db import models
 
+
 class Survey(models.Model):
     id = models.AutoField(primary_key=True)
     currentlyActive = models.BooleanField(default=False)
     survey_text = models.CharField(max_length=200)
-
 
 
 class SurveyQuestion(models.Model):
@@ -13,9 +13,9 @@ class SurveyQuestion(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+
 class SurveyChoice(models.Model):
     id = models.AutoField(primary_key=True)
     question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
--- sustainable_app/views/minigame_survey.py
+++ sustainable_app/views/minigame_survey.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponseBadRequest
 from django.shortcuts import render
-from django.db.models import Prefetch
 from django.shortcuts import redirect
 
-from sustainable_app.models.survey import SurveyQuestion,SurveyChoice, Survey
+from sustainable_app.models.survey import SurveyQuestion, SurveyChoice, Survey
+
 
 @login_required(login_url='/login')
 def minigame_survey(request):
 
-    active_survey = Survey.objects.filter(currentlyActive=True).first()    
+    active_survey = Survey.objects.filter(currentlyActive=True).first()
     questions = SurveyQuestion.objects.filter(Survey=active_survey)
     choices = SurveyChoice.objects.filter(question__in=questions)
     context = {'questions': questions, 'choices': choices}
@@ -17,21 +18,22 @@ def minigame_survey(request):
 
         strings_to_exclude = ('csrfmiddlewaretoken', 'survey-submit')
 
-        filtered_keys = [key for key in request.POST.keys() if key not in strings_to_exclude]
+        filtered_keys = [key for key in request.POST.keys()
+                         if key not in strings_to_exclude]
 
-        #Gets choice entry and adds to votes
+        # Gets choice entry and adds to votes
         for q_id in filtered_keys:
-            #Gets the id of the choice that needs to be added to
+            # Gets the id of the choice that needs to be added to
             choice_id_to_change = request.POST.get(q_id)
 
-            choice = SurveyChoice.objects.get(id = choice_id_to_change)
+            try:
+                choice = SurveyChoice.objects.get(id=choice_id_to_change)
 
-            choice.votes +=1
-            choice.save()
+                choice.votes += 1
+                choice.save()
+            except SurveyChoice.DoesNotExist:
+                return HttpResponseBadRequest()
 
         return redirect('home')
 
-
     return render(request, 'sustainable_app/minigame_survey.html', context)
-
-
